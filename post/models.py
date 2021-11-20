91c0bfc017d01840907e5ad0d2af175dcebe2dac
simo <alilousimo81@gmail.com>
2021-11-20 20:33:39 +0100

    add

--- post/models.py
+++ post/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.fields import TextField
+from django.db.models.fields import BooleanField, TextField
 from django.db.models.fields.files import ImageField
 from django.utils import timezone
 # Create your models here.
@@ -12,6 +12,7 @@ class Post(models.Model):
     content =  models.TextField(max_length=50)
     craet_at = models.DateTimeField(default=timezone.now())
     image =   models.ImageField(upload_to='images' )
+    active = BooleanField(default=False)
     
     
 
--- post/views.py
+++ post/views.py
@@ -8,8 +8,22 @@ from .models import Post
 # Create your views here.
 class PostList(ListView):
     model = Post
-    context_object_name = 'posts'
+    #context_object_name = 'posts'  ## in template [ object_list , post_list ]
+    ordering = ['craet_at']
+    # queryset = Post.objects.filter(active=True)
+    # template_name = 'post/test.html'
     
+    def get_queryset(self):
+       return Post.objects.filter(active=True)
+   
+   
+    def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       #you can call anther model her and geve it to context
+       context['my_name'] = 'Alilou mohamed'
+       return context
+   
+   
     
 class PostDetail(DetailView):
     model = Post
